86869c7ecb52c947f282a6e15de1e8e5b0522959
+Sam Thongo <+samuelthongo73@gmail.com>
2023-12-18 02:45:19 +0300

    Deployment

--- config.py
+++ config.py
@@ -16,13 +16,14 @@ class Config:
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///dev.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     DEBUG = True
     # SQLALCHEMY_ECHO=True
 
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///prod.db")
+    #postgres://recipe_app_project_user:uQRy7SNONJlXXnNecsX2xPYfjJzckcao@dpg-clvnmpug1b2c73cig1mg-a.oregon-postgres.render.com/recipe_app_project
     DEBUG = os.getenv("DEBUG", False)
     SQLALCHEMY_ECHO = os.getenv("ECHO", False)
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", False)
