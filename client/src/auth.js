6bdb78d86550f8a875557e1670ab2dcb98646eae
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-12-19 00:56:45 +0300

    Completed Project

--- client/src/auth.js
+++ client/src/auth.js
@@ -1,11 +1,14 @@
-import {createAuthProvider} from 'react-token-auth'
+// auth.js
+import { createAuthProvider } from 'react-token-auth';
 
-export const [useAuth, authFetch, login, logout] =
-    createAuthProvider({
-        accessTokenKey: 'access_token',
-        onUpdateToken: (token) => fetch('/auth/refresh', {
-            method: 'POST',
-            body: token.refresh_token
-        })
-        .then(r => r.json())
-    })
+export const [useAuth, authFetch, login, logout] = createAuthProvider({
+  accessTokenKey: 'access_token',
+  onUpdateToken: (token) =>
+    fetch('/auth/refresh', {
+      method: 'POST',
+      body: JSON.stringify({ refresh_token: token.refresh_token }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((r) => r.json()),
+});
--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import { Button, Card ,Modal} from 'react-bootstrap';
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 
+const Recipe = ({ title, description, onClick, onDelete }) => {
+  // Removed unused 'Modal' import
 
-const Recipe=({title,description,onClick,onDelete})=>{
-    return(
-        <Card className="recipe">
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <p>{description}</p>
-                <Button variant='primary' onClick={onClick}>Update</Button>
-                {' '}
-                <Button variant='danger' onClick={onDelete}>Delete</Button>
-            </Card.Body>
-        </Card>
-    )
-}
-
+  return (
+    <Card className="recipe">
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <p>{description}</p>
+        <Button variant="primary" onClick={onClick}>
+          Update
+        </Button>
+        {' '}
+        <Button variant="danger" onClick={onDelete}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default Recipe;
